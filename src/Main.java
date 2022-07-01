562c719e783677448147972ec893807b471ded26
ElenaR <llinusik@mail.ru>
2022-07-01 17:33:25 +0300

    Готова ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean result = Validator.validate("Nataha", "Hki_098jfjkI_k_k", "Hki_098jfjkI_k_k");
+        if (result) {
+            System.out.println("Логин и пароль верны");
+        } else {
+            System.out.println("Логин и/или пароль н/верны");
+        }
+
+
+
     }
 }
---
+++ src/Validator.java
@@ -0,0 +1,56 @@
+public class Validator {
+
+
+    private static final String VALID_CHARACTERS = "abcdifghijklmnopqrstuvwxyzABCDIFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+
+    private Validator() {
+    }
+
+    public static boolean validate(String login,
+                                   String password,
+                                   String confirmPassword) {
+        try {
+            checkUsingLoop(login, password, confirmPassword);
+            return true;
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+    }
+
+    private static void checkUsingLoop(String login,
+                                       String password,
+                                       String confirmPassword) throws WrongLoginException, WrongPasswordException {
+        if (login == null || login.length() > 20) {
+            throw new WrongLoginException("Длина логина должна быть меньше или равно 20");
+        }
+        if (password == null || !password.equals(confirmPassword)) {
+            throw new WrongPasswordException("Пароли должны совпадать");
+        }
+        if (password == null && password.length() >= 20) {
+            throw new WrongPasswordException("Длина пароля должна быть меньше 20");
+        }
+
+        checkCharacters(login, "Логин");
+        checkCharacters(password, "Пароль");
+    }
+
+    private static void checkCharacters(String s, String word) throws WrongLoginException {
+        for (int i = 0; i < s.length(); i++) {
+            if (!VALID_CHARACTERS.contains(String.valueOf(s.charAt(i)))) {
+                throw new WrongLoginException(String.format(" %s содержит неверный символ %c!", word, s.charAt(i)));
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
